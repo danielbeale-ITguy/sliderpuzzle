427118fa0e03c997c7aaf1631e4c7772cdb23462
danielbeale-ITguy <danielbeale719@gmail.com>
2025-05-29 20:36:21 +1000

    can move square on mulitple clicks to left

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const square = document.getElementsByClassName('square')[0];
+let squareposition = 0;
 
 square.addEventListener('click',mover)
 
+
 function mover(){
+    squareposition += 50;
     square.style.backgroundColor = 'blue'
-    square.style.left = '50px';
+    square.style.left = squareposition + 'px';
+
 }

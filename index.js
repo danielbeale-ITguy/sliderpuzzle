02595096223da0aed426d0a9a48d72e648d837ca
danielbeale-ITguy <danielbeale719@gmail.com>
2025-05-30 22:04:17 +1000

    have change from sqaure to number buttons on a grid, and now on click event moves button to number 9 buttons position

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-const square = document.getElementsByClassName('square')[0];
-let squareposition = 0;
+const invbutton = document.getElementsByClassName('button')[8];
+invbutton.style.opacity = 0.5;
+const invrect = invbutton.getBoundingClientRect();
 
-square.addEventListener('click',mover)
 
+const buttons = document.getElementsByClassName('button');
 
-function mover(){
-    squareposition += 50;
-    square.style.backgroundColor = 'blue'
-    square.style.left = squareposition + 'px';
+for (let i = 0; i < buttons.length; i++) {
+    if (i==8) continue;
+    buttons[i].addEventListener('click', changeColor);
 
 }
+
+function changeColor(event) {
+    
+
+
+    
+    event.target.style.position = 'absolute'
+    event.target.style.backgroundColor = 'pink'
+    event.target.style.left = `${invrect.left}px`;
+    event.target.style.top = `${invrect.top}px`;
+}
+
+
+
+
+
+
+
+
+
+const eightOriginalPosition = {
+    left: eightrect.left,
+    top: eightrect.top
+};
+
+function mover () {
+
+    eightbutton.style.position = 'absolute';
+    eightbutton.style.left = `${rect.left}px`;
+    eightbutton.style.top = `${rect.top}px`;
+
+    invbutton.style.position = 'absolute';
+    invbutton.style.left = `${eightOriginalPosition.left}px`;
+    invbutton.style.top = `${eightOriginalPosition.top}px`;
+}
+
+
+
+

93591db7ecc5af9b93f29259b444254da8f66843
danielbeale-ITguy <danielbeale719@gmail.com>
2025-05-31 19:56:22 +1000

    partial working puzzle buttons take position of invisible button, and invisible button take position of pressed button, however buttons can move around freely

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
 const invbutton = document.getElementsByClassName('button')[8];
-invbutton.style.opacity = 0.5;
-const invrect = invbutton.getBoundingClientRect();
+invbutton.style.opacity = 0;
+let invrect = invbutton.getBoundingClientRect();
 
 
 const buttons = document.getElementsByClassName('button');
 
 for (let i = 0; i < buttons.length; i++) {
     if (i==8) continue;
-    buttons[i].addEventListener('click', changeColor);
-
+    buttons[i].addEventListener('click', changePos);
+    
 }
 
-function changeColor(event) {
+function changePos(event) {
+
     
-
-
+    temprect = event.target.getBoundingClientRect();
     
     event.target.style.position = 'absolute'
     event.target.style.backgroundColor = 'pink'
     event.target.style.left = `${invrect.left}px`;
     event.target.style.top = `${invrect.top}px`;
-}
-
-
-
-
-
-
-
-
-
-const eightOriginalPosition = {
-    left: eightrect.left,
-    top: eightrect.top
-};
-
-function mover () {
-
-    eightbutton.style.position = 'absolute';
-    eightbutton.style.left = `${rect.left}px`;
-    eightbutton.style.top = `${rect.top}px`;
-
+    
     invbutton.style.position = 'absolute';
-    invbutton.style.left = `${eightOriginalPosition.left}px`;
-    invbutton.style.top = `${eightOriginalPosition.top}px`;
+    invbutton.style.left = `${temprect.left}px`;
+    invbutton.style.top = `${temprect.top}px`;
+
+    invrect = temprect
+    
 }
+
+
 
 
 

acbd45689711789724217ea1c1bff97a600ecfc8
danielbeale-ITguy <danielbeale719@gmail.com>
2025-06-02 20:56:52 +1000

    isolated X axis and implemented if clause to check that if tile distance is greater than 100 dont move

--- index.js
+++ index.js
@@ -12,22 +12,32 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 function changePos(event) {
+    
+        temprect = event.target.getBoundingClientRect();
+        let invrectX = (invrect.left + invrect.right) / 2;
+        let invrectY = (invrect.top + invrect.bottom) /2; 
+        let temprectX = (temprect.left + temprect.right) / 2;
+        let temprectY = (temprect.top + temprect.bottom) / 2;
+        let distance = invrectY - temprectY
+        
+        if (invrectX == temprectX && distance == 50 || distance == -50){
+        
+        
+        event.target.style.position = 'absolute'
+        event.target.style.backgroundColor = 'pink'
+        event.target.style.left = `${invrect.left}px`;
+        event.target.style.top = `${invrect.top}px`;
+        
+        invbutton.style.position = 'absolute';
+        invbutton.style.left = `${temprect.left}px`;
+        invbutton.style.top = `${temprect.top}px`;
 
-    
-    temprect = event.target.getBoundingClientRect();
-    
-    event.target.style.position = 'absolute'
-    event.target.style.backgroundColor = 'pink'
-    event.target.style.left = `${invrect.left}px`;
-    event.target.style.top = `${invrect.top}px`;
-    
-    invbutton.style.position = 'absolute';
-    invbutton.style.left = `${temprect.left}px`;
-    invbutton.style.top = `${temprect.top}px`;
-
-    invrect = temprect
+        invrect = temprect
+        }
     
 }
+
+
 
 
 
